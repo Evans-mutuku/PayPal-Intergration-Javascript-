2ccbbd5fd47b0897a515d1ea6a17f45ac8265b56
Evans-mutuku <evansnyamai98@gmail.com>
2023-04-03 16:24:14 +0300

    done with paypal intergration

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Adding paypal payment");
+function initPayPalButton() {
+  paypal
+    .Buttons({
+      style: {
+        shape: "rect",
+        color: "gold",
+        layout: "vertical",
+        label: "paypal",
+      },
+
+      createOrder: function (data, actions) {
+        const userInput = document.getElementById("donate-amount").value;
+        const paypalAmount = parseFloat(userInput) / 100;
+        return actions.order.create({
+          purchase_units: [
+            { amount: { currency_code: "USD", value: paypalAmount } },
+          ],
+        });
+      },
+
+      onApprove: function (data, actions) {
+        return actions.order.capture().then(function (orderData) {
+          // Full available details
+          console.log(
+            "Capture result",
+            orderData,
+            JSON.stringify(orderData, null, 2)
+          );
+
+          // Show a success message within this page, for example:
+          const element = document.getElementById("paypal-button-container");
+          element.innerHTML = "";
+          element.innerHTML = "<h3>Thank you for your payment!</h3>";
+
+          // Or go to another URL:  actions.redirect('thank_you.html');
+        });
+      },
+
+      onError: function (err) {
+        console.log(err);
+      },
+    })
+    .render("#paypal-button-container");
+}
+initPayPalButton();
